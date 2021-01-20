10e94da5cc19c5c12603936a816bc6d342f166fd
Aysun Emir <aysun.emir@gmail.com>
2021-01-20 16:59:08 +0300

    tracking service basic setup

--- app/src/main/java/com/aemir/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/aemir/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,11 +1,14 @@
 package com.aemir.runningapp.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.aemir.runningapp.R
 import com.aemir.runningapp.databinding.FragmentTrackingBinding
+import com.aemir.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.aemir.runningapp.services.TrackingService
 import com.aemir.runningapp.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,11 +25,20 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentTrackingBinding.bind(view).apply {
             mapView.onCreate(savedInstanceState)
+            btnToggleRun.setOnClickListener {
+                sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+            }
             mapView.getMapAsync {
                 map = it
             }
         }
     }
+
+    private fun sendCommandToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
+        }
 
     override fun onResume() {
         super.onResume()
