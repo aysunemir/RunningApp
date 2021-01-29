0bc65c7cdb59ffc4ed5d9f39fb5cfd061a002899
Aysun Emir <aysun.emir@gmail.com>
2021-01-29 22:35:49 +0300

    total run statistics & settings fragment

--- app/src/main/java/com/aemir/runningapp/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/aemir/runningapp/ui/fragments/StatisticsFragment.kt
@@ -1,14 +1,58 @@
 package com.aemir.runningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.aemir.runningapp.R
+import com.aemir.runningapp.databinding.FragmentStatisticsBinding
+import com.aemir.runningapp.other.TrackingUtility
 import com.aemir.runningapp.ui.viewmodels.StatisticsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.round
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
+    private lateinit var binding: FragmentStatisticsBinding
     private val viewModel: StatisticsViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentStatisticsBinding.bind(view)
+        subscribeToObservers()
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.totalTimeRun.observe(viewLifecycleOwner, {
+            it?.let {
+                val totalTimeRun = TrackingUtility.getFormattedStopWatchTime(it)
+                binding.tvTotalTime.text = totalTimeRun
+            }
+        })
+
+        viewModel.totalDistance.observe(viewLifecycleOwner, {
+            it?.let {
+                val km = it / 1000f
+                val totalDistance = round(km * 10f) / 10f
+                val totalDistanceString = "$totalDistance km"
+                binding.tvTotalDistance.text = totalDistanceString
+            }
+        })
+
+        viewModel.totalAvgSpeed.observe(viewLifecycleOwner, {
+            it?.let {
+                val avgSpeed = round(it * 10f) / 10f
+                val avgSpeedString = "$avgSpeed km/h"
+                binding.tvAverageSpeed.text = avgSpeedString
+            }
+        })
+
+        viewModel.totalCaloriesBurned.observe(viewLifecycleOwner, {
+            it?.let {
+                val totalCalories = "$it kcal"
+                binding.tvTotalCalories.text = totalCalories
+            }
+        })
+    }
 }
--- app/src/main/java/com/aemir/runningapp/ui/viewmodels/StatisticsViewModel.kt
+++ app/src/main/java/com/aemir/runningapp/ui/viewmodels/StatisticsViewModel.kt
@@ -5,6 +5,13 @@ import androidx.lifecycle.ViewModel
 import com.aemir.runningapp.repositories.MainRepository
 
 class StatisticsViewModel @ViewModelInject constructor(
-    val repository: MainRepository
+    private val repository: MainRepository
 ) : ViewModel() {
+
+    val totalTimeRun = repository.getTotalTimeInMillis()
+    val totalDistance = repository.getTotalDistance()
+    val totalCaloriesBurned = repository.getTotalCaloriesBurned()
+    val totalAvgSpeed = repository.getTotalAvgSpeed()
+
+    val runSortedByDate = repository.getAllRunsSortedByDate()
 }
